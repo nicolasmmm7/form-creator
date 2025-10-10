f05813d98c65d39bbc2492356713b5d9d16c3635
JuanSanguinoRin <juansebastiansari@ufps.edu.co>
2025-10-10 18:25:13 -0500

    Sanguino es pendejo

--- settings.py
+++ settings.py
@@ -6,10 +6,9 @@ environ.Env.read_env()  # Carga variables de .env
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
+        'NAME': env.db('DATABASE_URL')['NAME'],
         'CLIENT': {
             'host': env('DATABASE_URL'),
         }
     }
 }
-
-
