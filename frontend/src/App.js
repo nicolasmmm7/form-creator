d0d03c35728a266dd7e266d31e2297c845097195
AngelyDema <angelydema@gmail.com>
2025-10-15 23:05:27 -0500

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
