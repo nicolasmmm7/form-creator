9c02694503a48d649fdf7c863fd22859ad2dba7b
JuanPlay27 <juandavid_mr27@hotmail.com>
2025-10-19 23:58:49 -0500

    actualizacion pequeña

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const Home = () => {
   const navigate = useNavigate();
   const [formularios, setFormularios] = useState([]);
